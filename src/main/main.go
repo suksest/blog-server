dd5ec82cae06f8b87524e43ff89f1614b22638a8
ridwanfathin <m.ridwanfathin@gmail.com>
2018-07-19 16:57:51 +0700

    init connection to postgresql db

--- src/main/main.go
+++ src/main/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "router"
+	"database/sql"
+	"fmt"
+	"router"
+
+	_ "github.com/lib/pq"
 )
 
-func main() {
-    fmt.Println("Welcome to the webserver")
-    e := router.New()
+var db *sql.DB
 
-    e.Start(":8000")
+func main() {
+	fmt.Println("Welcome to the webserver")
+
+	e := router.New()
+
+	var err error
+	db, err = sql.Open("postgres", "postgres://airin:password@localhost/bookstore?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to your database.")
+
+	e.Start(":8000")
+
 }
