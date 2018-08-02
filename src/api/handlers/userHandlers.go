a0da6ca5935dfcfb8a0047cc37866ac1df73c151
Muhammad Ridwan Fathin <m.ridwanfathin@gmail.com>
2018-08-02 15:06:32 +0700

    add authorization to all method

--- src/api/handlers/userHandlers.go
+++ src/api/handlers/userHandlers.go
@@ -131,6 +131,7 @@ func LoginUser(c echo.Context) error {
 		panic(err)
 	}
 
+	fmt.Printf("user:" + res.Username + " " + fmt.Sprint(reply))
 	fmt.Printf("Ok: User '%s' logged in\n", res.Username)
 	return c.JSON(http.StatusOK, map[string]string{
 		"status":  "OK",
@@ -162,86 +163,115 @@ func GetAllUser(c echo.Context) error {
 
 func GetUserByID(c echo.Context) error {
 
-	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-	}
+	req := c.Request()
+	token := req.Header.Get("Authorization")
 
-	user, err := user.FindByID(uint(id))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-	}
+	if redis.Find(token) {
+		id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
+		}
 
-	return c.JSON(http.StatusOK, user)
+		user, err := user.FindByID(uint(id))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
+		}
+
+		return c.JSON(http.StatusOK, user)
+	} else {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"status":  "FORBIDDEN",
+			"message": "Invalid Token",
+		})
+	}
 }
 
 func DeleteUserByID(c echo.Context) error {
 
-	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
+	req := c.Request()
+	token := req.Header.Get("Authorization")
+
+	if redis.Find(token) {
+		id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
+		}
+
+		user.Delete(uint(id))
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"status":  "OK",
+			"message": "User with ID:" + fmt.Sprint(id) + " sucesfully deleted",
+		})
+	} else {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"status":  "FORBIDDEN",
+			"message": "Invalid Token",
+		})
 	}
-
-	user.Delete(uint(id))
-
-	return c.JSON(http.StatusOK, map[string]string{
-		"status":  "OK",
-		"message": "User with ID:" + fmt.Sprint(id) + " sucesfully deleted",
-	})
 }
 
 func UpdateUser(c echo.Context) error {
+	req := c.Request()
+	token := req.Header.Get("Authorization")
 
-	theUser := user.User{}
+	if redis.Find(token) {
+		theUser := user.User{}
 
-	defer c.Request().Body.Close()
+		defer c.Request().Body.Close()
 
-	b, err := ioutil.ReadAll(c.Request().Body)
-	if err != nil {
-		log.Printf("Failed reading the request body for update user: %s\n", err)
-		return c.JSON(http.StatusInternalServerError, "Failed reading the request body")
-	}
+		b, err := ioutil.ReadAll(c.Request().Body)
+		if err != nil {
+			log.Printf("Failed reading the request body for update user: %s\n", err)
+			return c.JSON(http.StatusInternalServerError, "Failed reading the request body")
+		}
 
-	err = json.Unmarshal(b, &theUser)
-	if err != nil {
-		log.Printf("Failed unmarshaling in update user: %s\n", err)
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"status":  "FAILED",
-			"message": "Failed unmarshaling input",
-		})
-	}
-
-	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-	}
-
-	res, err := user.Update(&theUser, uint(id))
-	if err != nil {
-		switch err.(type) {
-		case *user.UsernameDuplicateError:
-			fmt.Println("Bad Request: ", err.Error())
-			return c.JSON(http.StatusBadRequest, map[string]string{
-				"status":  "FAILED",
-				"message": "Bad Request",
-			})
-		case *user.EmailDuplicateError:
-			fmt.Println("Bad Request: ", err.Error())
-			return c.JSON(http.StatusBadRequest, map[string]string{
-				"status":  "FAILED",
-				"message": "Bad Request",
-			})
-		default:
-			fmt.Println("Internal Server Error: ", err.Error())
+		err = json.Unmarshal(b, &theUser)
+		if err != nil {
+			log.Printf("Failed unmarshaling in update user: %s\n", err)
 			return c.JSON(http.StatusInternalServerError, map[string]string{
 				"status":  "FAILED",
-				"message": "Internal Server Error",
+				"message": "Failed unmarshaling input",
 			})
 		}
+
+		id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
+		}
+
+		res, err := user.Update(&theUser, uint(id))
+		if err != nil {
+			switch err.(type) {
+			case *user.UsernameDuplicateError:
+				fmt.Println("Bad Request: ", err.Error())
+				return c.JSON(http.StatusBadRequest, map[string]string{
+					"status":  "FAILED",
+					"message": "Bad Request",
+				})
+			case *user.EmailDuplicateError:
+				fmt.Println("Bad Request: ", err.Error())
+				return c.JSON(http.StatusBadRequest, map[string]string{
+					"status":  "FAILED",
+					"message": "Bad Request",
+				})
+			default:
+				fmt.Println("Internal Server Error: ", err.Error())
+				return c.JSON(http.StatusInternalServerError, map[string]string{
+					"status":  "FAILED",
+					"message": "Internal Server Error",
+				})
+			}
+		}
+		fmt.Println("Updated: ", res.ID)
+		return c.JSON(http.StatusOK, map[string]string{
+			"status":  "OK",
+			"message": "User with ID: " + fmt.Sprint(res.ID) + " Sucessfully Updated",
+		})
+	} else {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"status":  "FORBIDDEN",
+			"message": "Invalid Token",
+		})
 	}
-	fmt.Println("Updated: ", res.ID)
-	return c.JSON(http.StatusOK, map[string]string{
-		"status":  "OK",
-		"message": "User with ID: " + fmt.Sprint(res.ID) + " Sucessfully Updated",
-	})
 }
