898ac75bcfd33d465d09772b79ce95509f8a5a7f
Muhammad Ridwan Fathin <m.ridwanfathin@gmail.com>
2018-08-03 11:23:27 +0700

    delete unnecessary

--- src/router/router.go
+++ src/router/router.go
@@ -11,19 +11,16 @@ func New() *echo.Echo {
 	e := echo.New()
 
 	// create groups
-	adminGroup := e.Group("/admin")
 	jwtGroup := e.Group("/jwt")
 
 	// set all middlewares
 	middlewares.SetMainMiddlewares(e)
-	middlewares.SetAdminMiddlewares(adminGroup)
 	middlewares.SetJwtMiddlewares(jwtGroup)
 
 	// set main routes
 	api.MainGroup(e)
 
 	// set group routes
-	api.AdminGroup(adminGroup)
 	api.JwtGroup(jwtGroup)
 
 	return e
