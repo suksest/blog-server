801a2b992d9e0ecf93ac1fc1a76a1e63ab19beef
Sukma Setyaji <suksest@live.com>
2018-08-13 16:01:54 +0700

    update refactoring anon and user limiter
    split limiter to anon and user limiter in package tokenbucket

--- src/api/middlewares/ratelimiter/tokenbucket/limiter.go
+++ src/api/middlewares/ratelimiter/tokenbucket/limiter.go
@@ -15,7 +15,7 @@ func Init(b *Bucket, id string) { //id can be username for authenticated user, o
 	defer r.Close()
 
 	// Init
-	_, err := r.Do("HMSET", b.Prefix+"_"+id, "tokens", b.Capacity, "ts", time.Now())
+	_, err := r.Do("HMSET", b.Prefix+"_"+id, "tokens", b.Capacity, "ts", time.Now().Unix())
 	if err != nil {
 		panic(err)
 	}
@@ -28,57 +28,52 @@ func SetHeader(c echo.Context, limit, remain uint) { //id can be username for au
 }
 
 //Limiter limit request
-func Limiter(config *Bucket) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			id := c.RealIP()
-			r := redis.RedisConnect()
-			defer r.Close()
+func Limiter(config *Bucket, c echo.Context, id string) bool {
+	r := redis.RedisConnect()
+	defer r.Close()
 
-			keys, err := r.Do("KEYS", config.Prefix+"_"+id)
-			if err != nil {
-				panic(err)
-			}
-			if fmt.Sprint(keys) == "[]" {
-				Init(config, id)
-				Take(id)
-			} else {
-				reqTime := time.Now().Unix()
-				if GetTokens(id) != 0 {
-					Take(id)
-					SetHeader(c, config.Capacity, GetTokens(id))
-					return next(c)
-				}
-				elapsedTime := GetElapsedTime(reqTime, GetLastRefillTimestamp(id))
-				tokensToBeAdded := GetTokensToBeAdded(elapsedTime, config.Period)
-				if tokensToBeAdded > 0 {
-					if tokensToBeAdded <= config.Capacity {
-						Refill(id, tokensToBeAdded)
-						Take(id)
-						SetHeader(c, config.Capacity, GetTokens(id))
-						return next(c)
-					}
-					Refill(id, config.Capacity)
-					Take(id)
-					SetHeader(c, config.Capacity, GetTokens(id))
-					return next(c)
-				}
-				SetHeader(c, config.Capacity, 0)
-				return echo.ErrForbidden
-			}
-			SetHeader(c, config.Capacity, GetTokens(id))
-			return next(c)
-		}
+	keys, err := r.Do("HGETALL", config.Prefix+"_"+id)
+	if err != nil {
+		panic(err)
 	}
+	if fmt.Sprint(keys) == "[]" {
+		Init(config, id)
+		Take(config.Prefix, id)
+	} else {
+		reqTime := time.Now().Unix()
+		if GetTokens(config.Prefix, id) != 0 {
+			Take(config.Prefix, id)
+			SetHeader(c, config.Capacity, GetTokens(config.Prefix, id))
+			return true
+		}
+		elapsedTime := GetElapsedTime(reqTime, GetLastRefillTimestamp(config.Prefix, id))
+		tokensToBeAdded := GetTokensToBeAdded(elapsedTime, config.Period)
+		if tokensToBeAdded > 0 {
+			if tokensToBeAdded <= config.Capacity {
+				Refill(config.Prefix, id, tokensToBeAdded)
+				Take(config.Prefix, id)
+				SetHeader(c, config.Capacity, GetTokens(config.Prefix, id))
+				return true
+			}
+			Refill(config.Prefix, id, config.Capacity)
+			Take(config.Prefix, id)
+			SetHeader(c, config.Capacity, GetTokens(config.Prefix, id))
+			return true
+		}
+		SetHeader(c, config.Capacity, 0)
+		return false
+	}
+	SetHeader(c, config.Capacity, GetTokens(config.Prefix, id))
+	return true
 }
 
 //GetTokens return current available token in bucket
-func GetTokens(id string) uint {
+func GetTokens(prefix, id string) uint {
 	var tokens uint
 	r := redis.RedisConnect()
 	defer r.Close()
 
-	reply, err := r.Do("HGET", "userlimiter_"+id, "tokens")
+	reply, err := r.Do("HGET", prefix+"_"+id, "tokens")
 	if err != nil {
 		panic(err)
 	}
@@ -90,39 +85,39 @@ func GetTokens(id string) uint {
 }
 
 //Refill bucket with token in certain period
-func Refill(id string, tokens uint) {
+func Refill(prefix, id string, tokens uint) {
 	r := redis.RedisConnect()
 	defer r.Close()
 
-	_, err := r.Do("HSET", "userlimiter_"+id, "tokens", tokens)
+	_, err := r.Do("HSET", prefix+"_"+id, "tokens", tokens)
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = r.Do("HSET", "userlimiter_"+id, "ts", time.Now().Unix())
+	_, err = r.Do("HSET", prefix+"_"+id, "ts", time.Now().Unix())
 	if err != nil {
 		panic(err)
 	}
 }
 
 //Take a token to permit a request
-func Take(id string) {
+func Take(prefix, id string) {
 	r := redis.RedisConnect()
 	defer r.Close()
 
-	_, err := r.Do("HINCRBY", "userlimiter_"+id, "tokens", -1)
+	_, err := r.Do("HINCRBY", prefix+"_"+id, "tokens", -1)
 	if err != nil {
 		panic(err)
 	}
 }
 
 //GetLastRefillTimestamp return bucket's last refill timestamp
-func GetLastRefillTimestamp(id string) int64 {
+func GetLastRefillTimestamp(prefix, id string) int64 {
 	var lastRefillTimestamp int64
 	r := redis.RedisConnect()
 	defer r.Close()
 
-	reply, err := r.Do("HGET", "userlimiter_"+id, "ts")
+	reply, err := r.Do("HGET", prefix+"_"+id, "ts")
 	if err != nil {
 		panic(err)
 	}
