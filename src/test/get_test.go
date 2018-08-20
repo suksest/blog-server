9bd1016ab7b6742aae15cd84a0bf4326e28f523f
Muhammad Ridwan Fathin <m.ridwanfathin@gmail.com>
2018-08-20 11:10:26 +0700

    benchmarking 4 algorithm

--- src/test/get_test.go
+++ src/test/get_test.go
@@ -7,29 +7,7 @@ import (
 
 var token string = "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiUmlkIiwiZXhwIjoxNTM0NzY5OTk3LCJqdGkiOiJtYWluX3VzZXJfaWQifQ.VCRKMNbUnW8VA2A-5Xv8jX5-fm8GDXL9Xwg1Ffy7Vj7wMkz8Ov97Lst2tliCdkstrMGBJd7XnweWF7nsON38sQ0"
 
-// func BenchmarkFixedWindowCounter(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		url := "http://localhost:8000/v1.0/auth/user/login"
-
-// 		var jsonStr = []byte(`{
-// 			"email": "rid@shade.com",
-// 			"password":"airin123"
-// 		}`)
-// 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-// 		req.Header.Set("X-Custom-Header", "myvalue")
-// 		req.Header.Set("Content-Type", "application/json")
-
-// 		client := &http.Client{}
-// 		resp, err := client.Do(req)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		defer resp.Body.Close()
-
-// 	}
-// }
-
-func BenchmarkFixedWindowCounter(b *testing.B) {
+func BenchmarkFixedWindowCounterGet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		url := "http://localhost:8000/v1.0/fwc/posts"
 
@@ -47,7 +25,7 @@ func BenchmarkFixedWindowCounter(b *testing.B) {
 	}
 }
 
-func BenchmarkSlidingWindowCounter(b *testing.B) {
+func BenchmarkSlidingWindowCounterGet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		url := "http://localhost:8000/v1.0/swc/posts"
 
@@ -65,9 +43,27 @@ func BenchmarkSlidingWindowCounter(b *testing.B) {
 	}
 }
 
-func BenchmarkSlidingWindowLog(b *testing.B) {
+func BenchmarkSlidingWindowLogGet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		url := "http://localhost:8000/v1.0/swl/posts"
+
+		req, err := http.NewRequest("GET", url, nil)
+		req.Header.Set("Authorization", token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
+
+func BenchmarkTokenBucketGet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url := "http://localhost:8000/v1.0/tb/posts"
 
 		req, err := http.NewRequest("GET", url, nil)
 		req.Header.Set("Authorization", token)
