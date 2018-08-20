7bf9e3f6bc4a9ccffd429b32ba63ce4b29a47a72
Muhammad Ridwan Fathin <m.ridwanfathin@gmail.com>
2018-08-20 11:31:41 +0700

    add post method benchmark

--- src/test/post_test.go
+++ src/test/post_test.go
@@ -1,23 +1,103 @@
 package test
 
-// func BenchmarkFixedWindowCounter(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		url := "http://localhost:8000/v1.0/auth/user/login"
+import (
+	"bytes"
+	"net/http"
+	"testing"
+)
 
-// 		var jsonStr = []byte(`{
-// 			"email": "rid@shade.com",
-// 			"password":"airin123"
-// 		}`)
-// 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-// 		req.Header.Set("X-Custom-Header", "myvalue")
-// 		req.Header.Set("Content-Type", "application/json")
+func BenchmarkFixedWindowCounterPost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url := "http://localhost:8000/v1.0/fwc/publish"
 
-// 		client := &http.Client{}
-// 		resp, err := client.Do(req)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		defer resp.Body.Close()
+		var jsonStr = []byte(`{
+			"AuthorID": 1,
+			"Body":     "Go golang rocks! ",
+			"Tags":     ["intro", "golang"]
+		}`)
 
-// 	}
-// }
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+		req.Header.Set("Authorization", token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
+
+func BenchmarkSlidingCounterPost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url := "http://localhost:8000/v1.0/swc/publish"
+
+		var jsonStr = []byte(`{
+			"AuthorID": 1,
+			"Body":     "Go golang rocks! ",
+			"Tags":     ["intro", "golang"]
+		}`)
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+		req.Header.Set("Authorization", token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
+
+func BenchmarkSlidingWindowLogPost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url := "http://localhost:8000/v1.0/swl/publish"
+
+		var jsonStr = []byte(`{
+			"AuthorID": 1,
+			"Body":     "Go golang rocks! ",
+			"Tags":     ["intro", "golang"]
+		}`)
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+		req.Header.Set("Authorization", token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
+
+func BenchmarkTokenBucketPost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		url := "http://localhost:8000/v1.0/tb/publish"
+
+		var jsonStr = []byte(`{
+			"AuthorID": 1,
+			"Body":     "Go golang rocks! ",
+			"Tags":     ["intro", "golang"]
+		}`)
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+		req.Header.Set("Authorization", token)
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+	}
+}
